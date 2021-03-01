54f0106d85adb13caec6a6448a2ac9d25c901bf1
hanseulhee <3021062@gmail.com>
2021-03-01 19:12:23 +0900

    first commit

---
+++ dropdown.js
@@ -0,0 +1,10 @@
+function menu(){
+    let click = document.querySelector(".drop-content");
+    if(click.style.display === "none"){
+        click.style.display = "block";
+
+    }else{
+        click.style.display = "none";
+
+    }
+}
